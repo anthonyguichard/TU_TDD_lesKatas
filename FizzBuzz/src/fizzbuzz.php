f7cf8c96155b7615035d1e8ca2a088f72eb8b52f
Antho <anthoguichard@MacBook-Pro-de-Antho.local>
2020-03-06 15:27:20 +0100

    GREEN FizzBuzz - final calcul

--- FizzBuzz/src/fizzbuzz.php
+++ FizzBuzz/src/fizzbuzz.php
@@ -25,8 +25,16 @@ class FizzBuzz
 		return $this->isFizz($number) || $this->isBuzz($number);
 	}
 
-	public function calc()
+	public function calc(int $count, array $result = [], int $iteration = 1)
 	{
-		//
+		if ($iteration > $count) return $result;
+
+		$result[$iteration] = $this->isFizzOrBuzz($iteration)
+				    ? $this->isFizz($iteration) . $this->isBuzz($iteration)
+				    : $iteration;
+
+		$iteration++;
+
+		return $this->calc($count, $result, $iteration);
 	}
 }
