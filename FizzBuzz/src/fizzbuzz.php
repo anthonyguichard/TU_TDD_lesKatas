8e9bf5816f1d1ce320a7105a5924b37264c61eec
Antho <anthoguichard@MacBook-Pro-de-Antho.local>
2020-03-06 15:00:28 +0100

    REFACTOR FizzBuzz - if number divisible by three

--- FizzBuzz/src/fizzbuzz.php
+++ FizzBuzz/src/fizzbuzz.php
@@ -3,6 +3,7 @@ namespace App;
 
 class FizzBuzz
 {
+	// if number is divisible by three is replaced by the word fizz
 	public function isFizz(int $number)
 	{
 		return $number % 3 === 0 
--- FizzBuzz/tests/fizzbuzzTest.php
+++ FizzBuzz/tests/fizzbuzzTest.php
@@ -5,6 +5,7 @@ use App\FizzBuzz;
 
 final class FizzBuzzTest extends TestCase
 {
+	// if number is divisible by three is replaced by the word fizz
 	public function test_a_number_divisible_by_three_is_replaced_by_the_word_fizz()
 	{
 		$fizz = new FizzBuzz();
